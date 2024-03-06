35eea2f0bc6b1c83c09b90ca8a5d8a7459c35a11
dom <dominik@powells.eu>
2024-03-06 20:56:14 +0000

    Store pin definitions in config file

---
+++ libs/pindef.h
@@ -0,0 +1,8 @@
+/*
+ * This file contains all pins used by the libraries.
+ * This is a poor substitute for something like a zephyr 
+ * device tree or a Kconfig, but it will have to do.
+ */
+
+#define NRF_CE_PIN 10
+#define NRF_CSN_PIN 9
