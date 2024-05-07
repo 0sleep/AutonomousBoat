79ce62c31fb0bc27398528354570cca2663b6851
dom <dominik@powells.eu>
2024-05-07 14:14:43 +0100

    Fix unicode issues and add magnetic north

--- libs/geo_utils.cpp
+++ libs/geo_utils.cpp
@@ -18,19 +18,19 @@ float distance_meters(float latA, float lonA, float latB, float lonB) {
 
 float convert_angle_radians(float angle) { return angle * (M_PI / 180.0); }
 
-float bearing(float latFrom, float lonFrom, float latTo, float lonTo) {
+float bearing_degrees(float latFrom, float lonFrom, float latTo, float lonTo) {
   /* From http://www.movable-type.co.uk/scripts/latlong.html#Bearing
    *
    * θ = atan2( sin Δλ ⋅ cos φ2 , cos φ1 ⋅ sin φ2 − sin φ1 ⋅ cos φ2 ⋅ cos Δλ )
    * where φ1,λ1 is the start point, φ2,λ2 the end point (Δλ is the
    * difference in longitude)
    */
-  float Δλ = convert_angle_radians(lonTo - lonFrom);
-  float φ1 = convert_angle_radians(latFrom);
-  float φ2 = convert_angle_radians(latTo);
-  const float y = sin(Δλ) * cos(φ2);
-  const float x = cos(φ1) * sin(φ2) - sin(φ1) * cos(φ2) * cos(Δλ);
-  const float θ = atan2(y, x);
-  const auto brng = (int)(θ * 180.0 / M_PI + 360.0) % 360;  // in degrees
+  float delta = convert_angle_radians(lonTo - lonFrom);
+  float phi1 = convert_angle_radians(latFrom);
+  float phi2 = convert_angle_radians(latTo);
+  const float y = sin(delta) * cos(phi2);
+  const float x = cos(phi1) * sin(phi2) - sin(phi1) * cos(phi2) * cos(delta);
+  const float theta = atan2(y, x);
+  const float brng = (int)(theta * 180.0 / M_PI + 360.0) % 360;  // in degrees
   return brng;
 }
--- libs/geo_utils.h
+++ libs/geo_utils.h
@@ -2,8 +2,14 @@
 #define GEO_UTILS
 #define USE_MATH_DEFINES
 #include "math.h"
+
+#define UINTCOORDS(x) (x/10000000)
+//From https://www.ncei.noaa.gov/products/wandering-geomagnetic-poles
+//This is from the WMM2020 representation
+#define MAG_NORTH_LAT 80.73
+#define MAG_NORTH_LON -72.68
 // determine distance to a target in meters
-// The implementation of this function is taken from
+// The implementation of this function is adapted from
 // https://github.com/AhiyaHiya/haversine, which is a c++ rewrite of the
 // javascript example on http://www.movable-type.co.uk/scripts/latlong.html
 float distance_meters(float latA, float lonA, float latB, float lonB);
